d393f5da237f5f78a8ad6b0e6120c9ee5c209485
hack086 <zhalil.p.1303@gmail.com>
2020-09-23 18:49:17 +0300

    first commit

--- while.py
+++ while.py
@@ -1,16 +1,14 @@
 x = 600
-while True:
-    x -=1
-    if x < 20:
-        x += 1
-    print(x)
+while x > 0:
+    if x < 40:
+        x -= 1
+    print (x)
 
-y = 600
+
     while True:
         x -= 1
         if x < 40:
             x += 1
         print(x)
-
 
 
