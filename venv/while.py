8b5bad149996fd994e11492121e3390ac54ca8e5
hack086 <zhalil.p.1303@gmail.com>
2020-09-23 16:02:17 +0300

    first commit

---
+++ venv/while.py
@@ -0,0 +1,16 @@
+x = 600
+while True:
+    x -=1
+    if x < 20:
+        x += 1
+    print(x)
+
+y = 600
+    while True:
+        x -= 1
+        if x < 40:
+            x += 1
+        print(x)
+
+
+
